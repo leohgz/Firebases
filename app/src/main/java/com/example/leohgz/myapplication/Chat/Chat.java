9ecc6c7effc36d14646e47160f33092f5ca1322c
Leonardo <leonardo.hernandez@oppous.com>
2016-07-22 16:54:11 -0400

    Se agrega funcionalidad.

---
+++ app/src/main/java/com/example/leohgz/myapplication/Chat/Chat.java
@@ -0,0 +1,15 @@
+package com.example.leohgz.myapplication.Chat;
+
+/**
+ * Created by Leonardo on 22/7/2016.
+ */
+public class Chat {
+    private String chatID;
+
+    public Chat(){
+
+    }
+
+
+
+}
